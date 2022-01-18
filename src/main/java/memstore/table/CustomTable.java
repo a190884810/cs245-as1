dbb93bddfd11a939c140c97de274aced0d5218f4
tiancheng <a190884810@gmail.com>
2022-01-17 18:39:15 -0800

    Implement rowSum cache for CustomTable, passed wide bench test but fail the narrow bench test.

--- src/main/java/memstore/table/CustomTable.java
+++ src/main/java/memstore/table/CustomTable.java
@@ -15,9 +15,9 @@ public class CustomTable implements Table {
     protected int numRows;
     protected int numCols;
     protected int sumCol0;
-    protected ByteBuffer rowsBuffer;
+    protected ByteBuffer columnsBuffer;
     protected TreeMap<Integer, IntArrayList> firstIndex;
-    protected TreeMap<Pair, IntArrayList> secondIndex;
+    protected TreeMap<Pair, Integer> secondIndex;
 
     // rowSum Cache
     protected ByteBuffer rowSums;
@@ -38,13 +38,18 @@ public class CustomTable implements Table {
         this.numCols = loader.getNumCols();
         List<ByteBuffer> rows = loader.getRows();
         this.numRows = rows.size();
-        this.rowsBuffer = ByteBuffer.allocate(ByteFormat.FIELD_LEN * numRows * numCols);
+        this.columnsBuffer = ByteBuffer.allocate(ByteFormat.FIELD_LEN * numRows * numCols);
         this.rowSums = ByteBuffer.allocate(ByteFormat.FIELD_LEN * numRows);
 
         for (int rowId = 0; rowId < numRows; rowId++) {
             ByteBuffer curRow = rows.get(rowId);
-            this.rowsBuffer.put(curRow);
+            for (int colId = 0; colId < numCols; colId++) {
+                int offset = ByteFormat.FIELD_LEN * ((colId * numRows) + rowId);
+                this.columnsBuffer.putInt(offset, curRow.getInt(ByteFormat.FIELD_LEN * colId));
+            }
+        }
 
+        for (int rowId = 0; rowId < numRows; rowId++) {
             int col0Val = getIntField(rowId, 0);
             sumCol0 += col0Val;
             // Build first index cache
@@ -54,7 +59,7 @@ public class CustomTable implements Table {
             int col1Val = getIntField(rowId, 1);
             int col2Val = getIntField(rowId, 2);
             Pair col12Val = new Pair(col1Val, col2Val);
-            addSecondIndex(col12Val, rowId);
+            addSecondIndex(col12Val, col0Val);
 
             // Setup rowSum cache
             int curRowSum = 0;
@@ -71,8 +76,8 @@ public class CustomTable implements Table {
      */
     @Override
     public int getIntField(int rowId, int colId) {
-        int offset = ByteFormat.FIELD_LEN * ((rowId * numCols) + colId);
-        return rowsBuffer.getInt(offset);
+        int offset = ByteFormat.FIELD_LEN * ((colId * numRows) + rowId);
+        return columnsBuffer.getInt(offset);
     }
 
     /**
@@ -86,26 +91,34 @@ public class CustomTable implements Table {
             deleteFirstIndex(oldField, rowId);
             addFirstIndex(field, rowId);
             sumCol0 += field - oldField;
+
+            // update secondIndex cache
+            int col1Val = getIntField(rowId, 1);
+            int col2Val = getIntField(rowId, 2);
+            Pair pair = new Pair(col1Val, col2Val);
+            deleteSecondIndex(pair, oldField);
+            addSecondIndex(pair, field);
         } else if (colId == 1 || colId == 2) {
             Pair col12Val;
             Pair updatedKeyPair;
 
+            int col0Val = getIntField(rowId, 0);
             // Delete and add reference
             if (colId == 1) {
                 int col2Val = getIntField(rowId, 2);
                 col12Val = new Pair(oldField, col2Val);
-                deleteSecondIndex(col12Val, rowId);
+                deleteSecondIndex(col12Val, col0Val);
                 updatedKeyPair = new Pair(field, col2Val);
             } else {
                 int col1Val = getIntField(rowId, 1);
                 col12Val = new Pair(col1Val, oldField);
-                deleteSecondIndex(col12Val, rowId);
+                deleteSecondIndex(col12Val, col0Val);
                 updatedKeyPair = new Pair(col1Val, field);
             }
-            addSecondIndex(updatedKeyPair, rowId);
+            addSecondIndex(updatedKeyPair, col0Val);
         }
-        int offset = ByteFormat.FIELD_LEN * ((rowId * numCols) + colId);
-        rowsBuffer.putInt(offset, field);
+        int offset = ByteFormat.FIELD_LEN * ((colId * numRows) + rowId);
+        columnsBuffer.putInt(offset, field);
 
         // Update rowSum cache
         int oldRowSum = rowSums.getInt(ByteFormat.FIELD_LEN * rowId);
@@ -133,7 +146,7 @@ public class CustomTable implements Table {
     @Override
     public long predicatedColumnSum(int threshold1, int threshold2) {
         long sum = 0;
-        for (Map.Entry<Pair, IntArrayList> entry : secondIndex.entrySet()) {
+        for (Map.Entry<Pair, Integer> entry : secondIndex.entrySet()) {
             Pair keyPair = entry.getKey();
             // check the threshold
             int col1Val = keyPair.getCol1Val();
@@ -144,10 +157,8 @@ public class CustomTable implements Table {
             if (col2Val >= threshold2) {
                 continue;
             }
-            IntArrayList rowIds = entry.getValue();
-            for (int i = 0; i < rowIds.size(); i++) {
-                sum += getIntField(rowIds.getInt(i), 0);
-            }
+            int rangeCol0Sum = entry.getValue();
+            sum += rangeCol0Sum;
         }
         return sum;
     }
@@ -217,21 +228,14 @@ public class CustomTable implements Table {
         }
     }
 
-    private void addSecondIndex(Pair keyPair, int rowId) {
-        IntArrayList rowIds = secondIndex.getOrDefault(keyPair, null);
-        if (rowIds == null) {
-            rowIds = new IntArrayList();
-            secondIndex.put(keyPair, rowIds);
-        }
-        rowIds.add(rowId);
+    private void addSecondIndex(Pair keyPair, int Col0Val) {
+        int rangeCol0Sum = secondIndex.getOrDefault(keyPair, 0);
+        secondIndex.put(keyPair, rangeCol0Sum + Col0Val);
     }
 
-    private void deleteSecondIndex(Pair keyPair, int rowId) {
-        IntArrayList rowIds = secondIndex.get(keyPair);
-        rowIds.rem(rowId);
-        if(rowIds.size() == 0) {
-            secondIndex.remove(keyPair);
-        }
+    private void deleteSecondIndex(Pair keyPair, int Col0Val) {
+        int rangeCol0Sum = secondIndex.getOrDefault(keyPair, 0);
+        secondIndex.put(keyPair, rangeCol0Sum - Col0Val);
     }
 
     /**
